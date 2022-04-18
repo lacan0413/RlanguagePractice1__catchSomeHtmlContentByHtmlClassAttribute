4cc869989b80dac054f8700c1aa796755caa688f
luka1901 <zga655426@gmail.com>
2022-04-19 01:59:31 +0800

    first commit

---
+++ Rpractice1-catchSomeHtmlContentByHtmlClassAttribute.r
@@ -0,0 +1,5 @@
+library(rvest)
+getWord = htmlParse(getURL("https://www.cbc.ca/natureofthings/features/6-most-iconic-canadian-animals-from-the-nature-of-things"))
+getContent=' //*[contains(concat( " ",@class," " ),concat( " ","entry-excerpt "," " ))]'
+xpathSApply(getWord,getContent)
+
